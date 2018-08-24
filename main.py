0663aeab1d5f87c8e6b0c69f64252c5418e0b211
henriquejdc <henriquejdc@localhost.localdomain>
2018-08-24 17:02:45 -0300

    Python Leitura

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ arq = open('case.txt', 'r')
 texto = arq.readlines()
 for linha in texto :
     pontos = linha.split(" ")
+    comentado = pontos[0]
 
-    if(pontos[0]=="Boundary"):
-        print(pontos)
-        board_left = pontos[2]
-        board_right = pontos[3]
-    else:
-        if(pontos[0]=="RoutedVia"):
-            print("R")
-            bondary = pontos[1]
-            print(bondary[1])
+    if(comentado[0] != "#"):
+        if(pontos[0]=="Boundary"):
+            print(pontos)
+            board_left = pontos[2]
+            board_right = pontos[3]
         else:
-            print("RS")
-            bondary = pontos[1]
-            print(bondary[1])
-            z.append(int(bondary[1]))
+            if(pontos[0]=="RoutedVia"):
+                print("R")
+                bondary = pontos[1]
+                print(bondary[1])
+            else:
+                print("RS")
+                bondary = pontos[1]
+                print(bondary)
+                print(bondary[1])
+                z.append(int(bondary[1]))
 arq.close()

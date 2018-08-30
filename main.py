cffda62ab8c707bca06626eb12e7302d3a4114ff
Henrique Dalla Corte <henriquejdc@localhost.localdomain>
2018-08-30 14:16:07 -0300

    Obs

--- main.py
+++ main.py
@@ -4,9 +4,13 @@
 layer = []
 #Bord Left
 bondary_coord = []
+obst_x = []
+obst_y = []
+dist_obj = 5
 
 arq = open('case.txt', 'r')
 texto = arq.readlines()
+
 for linha in texto :
     pontos = linha.split(" ")
     comentado = pontos[0]
@@ -14,7 +18,7 @@ for linha in texto :
     #Ignore Comments
     if(comentado[0] != "#"):
         if(pontos[0]=="Boundary"): #bondary
-            print(pontos)
+            #print(pontos)
             board_left = pontos[2]
             board_right = pontos[3]
 
@@ -23,8 +27,6 @@ for linha in texto :
             pontos_usado_left = pontos_usado1[1].split(",")
             bondary_coord.append(int(pontos_usado_left[0]))
             bondary_coord.append(int(pontos_usado_left[1]))
-
-
             pontos_usado = board_right.split(")")
             pontos_usado1 = pontos_usado[0].split("(")
             pontos_usado_right = pontos_usado1[1].split(",")
@@ -44,26 +46,40 @@ for linha in texto :
                 bondary = pontos[1]
             else:#Shapes and Obstacle
 
-                #if(pontos[0]=="RoutedShape"):
+                if(pontos[0]=="Obstacle"):
+                    bondary = pontos[1]
+                    pontos_usados = pontos[2].split(",")
+                    pontos_usados1 = pontos[3].split(",")
+                    ponto_x = pontos_usados[0].split("(")
+                    ponto_y = pontos_usados[1].split(")")
+                    obst_x.append(int(ponto_x[1]))
+                    obst_y.append(int(ponto_y[0]))
+                    ponto_x = pontos_usados1[0].split("(")
+                    ponto_y = pontos_usados1[1].split(")")
+                    obst_x.append(int(ponto_x[1]))
+                    obst_y.append(int(ponto_y[0]))
 
-                #if(pontos[0]=="Obstacle"):
-
-                bondary = pontos[1]
-                pontos_usados = pontos[2].split(",")
-                pontos_usados1 = pontos[3].split(",")
-                ponto_x = pontos_usados[0].split("(")
-                ponto_y = pontos_usados[1].split(")")
-                layer[int(bondary[1])-1][0].append(int(ponto_x[1]))
-                layer[int(bondary[1])-1][1].append(int(ponto_y[0]))
-                ponto_x = pontos_usados1[0].split("(")
-                ponto_y = pontos_usados1[1].split(")")
-                layer[int(bondary[1])-1][0].append(int(ponto_x[1]))
-                layer[int(bondary[1])-1][1].append(int(ponto_y[0]))
+                else:
+                    bondary = pontos[1]
+                    pontos_usados = pontos[2].split(",")
+                    pontos_usados1 = pontos[3].split(",")
+                    ponto_x = pontos_usados[0].split("(")
+                    ponto_y = pontos_usados[1].split(")")
+                    layer[int(bondary[1])-1][0].append(int(ponto_x[1]))
+                    layer[int(bondary[1])-1][1].append(int(ponto_y[0]))
+                    ponto_x = pontos_usados1[0].split("(")
+                    ponto_y = pontos_usados1[1].split(")")
+                    layer[int(bondary[1])-1][0].append(int(ponto_x[1]))
+                    layer[int(bondary[1])-1][1].append(int(ponto_y[0]))
 arq.close()
 
 for bond in layer:
     for lay in bond:
         lay.sort()
+print bondary_coord
 
 for bond in layer:
     print(bond)
+
+print obst_x
+print obst_y

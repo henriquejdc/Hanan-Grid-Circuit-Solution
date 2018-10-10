2b48368a65cc8277e7d622f80e57eb2460664de4
Henrique Jose <riquejdc@gmail.com>
2018-10-10 19:55:12 -0300

    for teste

--- chamada.py
+++ chamada.py
@@ -1,6 +1,6 @@
 import os
 import sys
-a = '/usr/bin/time --verbose -o times/time.txt python2 teste.py cases/case2.txt'
-b = '/usr/bin/time --verbose -o times/time1.txt python2 teste.py cases/case2.txt'
-os.system(a)
-os.system(b)
+number_test = 2
+for x in range(number_test):
+    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 teste.py cases/case' + str(x) + '.txt'
+    os.system(chamada)

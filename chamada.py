e578c6782361bd6b686bdcd7f138dccb349540ff
Henrique Dalla Corte <henriquejdc@localhost.localdomain>
2018-10-11 17:05:16 -0300

    ajeitando

--- chamada.py
+++ chamada.py
@@ -2,5 +2,5 @@ import os
 import sys
 number_test = 2
 for x in range(number_test):
-    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 teste.py cases/case' + str(x) + '.txt'
+    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 main.py cases/case' + str(x) + '.txt'
     os.system(chamada)

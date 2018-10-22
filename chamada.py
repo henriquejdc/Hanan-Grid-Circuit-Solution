fc98910d31948fa144ab6541e977552755f2a24c
Henrique Dalla Corte <henriquejdc@localhost.localdomain>
2018-10-22 16:39:29 -0300

    cases e fazer a arvore de segmento

--- chamada.py
+++ chamada.py
@@ -1,6 +1,8 @@
 import os
 import sys
-number_test = 2
+number_test = 3
 for x in range(number_test):
-    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 main.py cases/case' + str(x) + '.txt'
+    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 main.py cases/cases_m/case' + str(x) + '.txt'
+    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 main.py cases/cases_o/case' + str(x) + '.txt'
+    chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python2 main.py cases/cases_c/case' + str(x) + '.txt'
     os.system(chamada)

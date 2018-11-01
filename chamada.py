228a613321070125c8d9e3ff03dd91f8ebe6e0a8
Henrique Dalla Corte <henriquejdc@localhost.localdomain>
2018-11-01 16:34:21 -0300

    arrumando

--- chamada.py
+++ chamada.py
@@ -3,4 +3,5 @@ import sys
 number_test = 10
 for x in range(number_test):
     chamada = '/usr/bin/time --verbose -o times/time' + str(x) + '.txt python main.py cases/case' + str(x) + '.txt out/out' + str(x)
+    print ('/usr/bin/time --verbose -o times/time' + str(x) + '.txt python main.py cases/case' + str(x) + '.txt out/out' + str(x))
     os.system(chamada)
